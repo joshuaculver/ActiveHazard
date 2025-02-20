e66156fb6db22a234d278bacb6c39691eeb713fa
Joshua Culver <joshculver1@gmail.com>
2025-02-20 18:17:36 -0500

    cleaning, build settings

---
+++ Assets/Scripts/SimpleAttached/FloorScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorScript : MonoBehaviour
+{
+    public int id;
+    public AudioClip[] steps;
+    public float spdMult;
+}
